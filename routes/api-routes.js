582d0b0577781a5101d1ce8f102a51adbf1a8431
Som Ramnani <som.ramnani@gmail.com>
2019-11-19 16:14:51 -0600

    Update api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -134,9 +134,7 @@ module.exports = function(app) {
   });
 
   // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
+  
   app.post("/api/clear", function(req, res) {
     // Empty out the arrays of data
     tableData.length = 0;
