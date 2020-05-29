633d83c51e3f143b8e8ee8b827e01d2824424167
Som Ramnani <som.ramnani@gmail.com>
2020-05-29 12:31:16 -0400

    adds mocktest for twilio's API

--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -2,10 +2,10 @@ var request = require("request");
 
 var base_url = "http://localhost:8080/";
 
-describe("Restaurant Server", function() {
-  describe("GET /", function() {
-    it("returns status code 200", function() {
-      request.get(base_url, function(error, response, body) {
+describe("Restaurant Server", function () {
+  describe("GET /", function () {
+    it("returns status code 200", function () {
+      request.get(base_url, function (error, response, body) {
         expect(response.statusCode).toEqual(200);
         done();
       });
--- spec/twilioSpec.js
+++ spec/twilioSpec.js
@@ -1,7 +1,16 @@
-describe("My Jasmine Setup", function() {
-  var a = false;
+var request = require("request");
+// var myMockTest = require("./mockTest");
+const user = "AC0cd1dd92db1f221da9dcf8d41dcc2311";
+const password = "5011d4d126b7c208206faa6abed97707";
 
-  it("tests if the value of a is true", function() {
-    expect(a).toBe(true);
+var base_url =
+  "https://api.twilio.com/2010-04-01/Accounts/AC0cd1dd92db1f221da9dcf8d41dcc2311/Messages";
+
+describe("Twilio App", function () {
+  it("should send sms", function () {
+    // var statusCode = myMockTest.statusCode;
+    request.post(base_url(user, password), function (error, response, body) {
+      expect(response.status).toMatch("");
+    });
   });
 });
